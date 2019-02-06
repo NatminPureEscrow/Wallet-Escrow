9e92201981ce347cbee86ba2f5958065c79b48d9
Natmin Pure Escrow <39843150+natminpe@users.noreply.github.com>
2019-02-06 21:45:31 +1000

    Add files via upload

--- EscrowWallet.sol
+++ EscrowWallet.sol
@@ -1,106 +1,49 @@
 pragma solidity ^0.4.22;
 
-import "./Ownable.sol";
-
-contract ERC223 {
-
-	// ERC20 standard function
-    function transfer(address _to, uint256 _value) public returns (bool success);
-    
-    // ERC223 Standard functions
-}
-
-contract NatminApp is Ownable {
-
-	// Natmin transaction interface	
-	function updateTransactionBuyerPaid(uint256 _transID) public ;
-	function getTransactionAmount(uint256 _transID) public view returns (uint256);
-    function getTransactionBuyerPaid(uint256 _transID) public view  returns (bool);
-    function getTransactionSeller(uint256 _transID) public view returns (address);
-    function getSellerAmount(uint256 _transID) public view returns (uint256);
-    function getTransactionBuyer(uint256 _transID) public view returns (address);
-}
+import "./GeneralContract.sol";
 
 contract EscrowWallet is Ownable {
 	using SafeMath for uint256;
 	
-	string 	private walletPassword;
-	uint256 private	transactionID;
+	GeneralContract settings;
 
-	address public  tokenContractAddress; // Wallet currency/token address
-	address public  appContractAddress;
-	address public	systemWalletAddress;
-	uint256 public  balance;
+	constructor (address _generalContractAddress) public {		
+		require(_generalContractAddress != address(0));
 
-	NatminApp public  appContract;
-	ERC223 	public tokenContract;
-
-	constructor (
-		string _walletPassword,
-		address _appContractAddress,
-		address _tokenContractAddress,
-		address _systemWalletAddress) public {		
-
-		require(bytes(_walletPassword).length > 0);
-		require(_appContractAddress != address(0));
-		require(_tokenContractAddress != address(0));
-		require(_systemWalletAddress != address(0));
-
-		walletPassword = _walletPassword;
-		appContractAddress = _appContractAddress;
-		tokenContractAddress = _tokenContractAddress; 
-		systemWalletAddress = _systemWalletAddress;
-		appContract  = NatminApp(_appContractAddress);
-		tokenContract = ERC223(_tokenContractAddress);
-		balance = 0;
+		settings = GeneralContract(_generalContractAddress);
 		transactionID = 0;
 	}
 
-	// Token fallback required for ERC223 standard
+	// Token fallback required for ERC20Standard standard
 	function tokenFallback(address _from, uint256 _value, bytes _data) public {
-		require(tokenContractAddress == msg.sender); // Only allows tokens from the agreed wallet currency
-		balance = balance.add(_value);
-		uint256 _transacionAmount = appContract.getTransactionAmount(transactionID);
-		bool _buyerPaid = appContract.getTransactionBuyerPaid(transactionID);
-		
-		// Check if transaction is already marked as paid and  
-		// Will stop receiving tokens once transaction is paid
-		require(_buyerPaid != true); 
-		if(balance >= _transacionAmount) {
-			appContract.updateTransactionBuyerPaid(transactionID); // Mark transaction as paid
-		}
+		address _tokenAddress = settings.getSettingAddress('TokenContract');
+		require(msg.sender == _tokenAddress);
 	}
 
-	function updateTransactionID(uint256 _transID) public returns (bool) {
-		require(appContractAddress == msg.sender); // Can only be called from the app contract
-		require(transactionID == 0); // Requires the transaction ID to be 0, can only be updated once
-		require(_transID != 0);
-		transactionID = _transID;
+	// Transfer function of the escrow wallet
+	function transfer(address _to, uint256 _amount) public returns (bool){
+		// To make sure the function is only called from the app contract or by the owner
+		address _transactionContractAddress = settings.getSettingAddress('TransactionContract');
+		require((msg.sender == _transactionContractAddress) || (msg.sender == contractOwner));
+		address _tokenAddress = settings.getSettingAddress('TokenContract');
+		ERC20Standard _tokenContract = ERC20Standard(_tokenAddress);
+		require(_tokenContract.transfer(_to, _amount));
 		return true;
 	}
 
-	// Checks the status of the transaction and pays the seller the required tokens
-	function transferTransactionAmounts(string _password) public returns (bool) {
-		// To make sure the function is only called from the app contract
-		require(appContractAddress == msg.sender);
-		require(keccak256(walletPassword) == keccak256(_password));		
-		 
-		uint256 _tokenAmount = appContract.getTransactionAmount(transactionID);
-		uint256 _sellerAmount = appContract.getSellerAmount(transactionID);
-		address _sellerAddress = appContract.getTransactionSeller(transactionID);
-		uint256 _feeAmount = _tokenAmount.sub(_sellerAmount);
-		tokenContract.transfer(_sellerAddress,_sellerAmount);
-		tokenContract.transfer(systemWalletAddress,_feeAmount);
-
-		return true;
-	}
-
-	function getTransactionID() public view returns (uint256) {
-		return transactionID;
+	// Returns the escrow wallet balance
+	function balance() public view returns (uint256) {
+		address _tokenAddress = settings.getSettingAddress('TokenContract');
+		ERC20Standard _tokenContract = ERC20Standard(_tokenAddress);
+		return _tokenContract.balanceOf(this);
 	}
 
 	// Destroying the wallet and returns the contents to the owner
 	function destroyWallet(address _owner) public ownerOnly {
+		address _tokenAddress = settings.getSettingAddress('TokenContract');
+		ERC20Standard _tokenContract = ERC20Standard(_tokenAddress);
+		require(_tokenContract.balanceOf(this) <= 0);
+
 		selfdestruct(_owner);
 	}
 }
---
+++ GeneralContract.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.4.22;
+
+import "./Ownable.sol";
+
+//ERC20 Standard interface specification
+contract ERC20Standard {
+    function balanceOf(address _user) public view returns (uint256 balance);
+    function transfer(address _to, uint256 _value) public returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+    function approve(address _spender, uint256 _value) public returns (bool success);
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+
+// This contact sets the list of general settings used by all contracts 
+contract GeneralContract is Ownable {
+	using SafeMath for uint256;
+
+	struct SettingDetail {
+		address settingAddress;
+		string settingDescription;		
+	}
+
+	mapping(string => SettingDetail) internal settings;
+
+    constructor () public {
+		contractOwner = msg.sender;
+	} 
+	
+	// Update a specific setting
+	function setSetting(string _name, address _address, string _description) public ownerOnly {
+		settings[_name].settingAddress = _address;
+		settings[_name].settingDescription = _description;
+	}
+
+	// Returns the specific setting's address
+	function getSettingAddress(string _name) public view returns (address) {
+		return settings[_name].settingAddress;
+	}
+
+	// Returns the specific setting's description 
+	function getSettingDescription(string _name) public view returns (string) {
+		return settings[_name].settingDescription;
+	}
+}
